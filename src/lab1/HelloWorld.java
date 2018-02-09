7cacd1438eb4270ea9bb7856e3d552976f36198c
sytseae <sytseae@csl4wk47.win2k.cs.ust.hk>
2018-02-09 14:39:30 +0800

    Display my name and student ID.

--- src/lab1/HelloWorld.java
+++ src/lab1/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("My name is Tse Shing Yan and my student ID is 20317851."); // Display the string.
     }
 }
